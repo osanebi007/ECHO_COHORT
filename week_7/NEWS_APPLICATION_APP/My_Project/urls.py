f639d1ff1c702d6b82807cdc7f3a8ff271cc05d2
Jesse <jeseosanebi@gmail.com>
2025-09-24 15:52:02 +0100

    Fix ALLOWED_HOSTS entry and update Swagger UI path in URLs

--- week_7/NEWS_APPLICATION_APP/My_Project/urls.py
+++ week_7/NEWS_APPLICATION_APP/My_Project/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
-        'api/schema/swagger-ui/',
+        '',
         SpectacularSwaggerView.as_view(url_name='schema'),
         name='swagger-ui',
     ),
